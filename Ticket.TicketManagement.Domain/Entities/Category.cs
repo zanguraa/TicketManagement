67af242977d5175d72d5352c663c7a9f3b9598b1
zangura <zanguraa@gmail.com>
2024-06-25 16:41:03 +0400

    edit project structure config

---
+++ Ticket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,11 @@
+﻿using Ticket.TicketManagement.Domain.Common;
+
+namespace Ticket.TicketManagement.Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Event>? Events { get; set; }
+    }
+}
---
+++ Ticket.TicketManagement.Domain/Entities/Order.cs
@@ -0,0 +1,11 @@
+﻿namespace Ticket.TicketManagement.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public int OrderTotal { get; set; }
+        public DateTime OrderPlaced { get; set; }
+        public bool OrderPaid { get; set; }
+    }
+}
