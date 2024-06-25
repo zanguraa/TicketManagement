3def438752db9a92ea3c6ec9db8c4c97b210ca85
zangura <zanguraa@gmail.com>
2024-06-25 21:16:11 +0400

    added Interfaces in Presistence

---
+++ src/Core/Ticket.TicketManagement.Application/Contracts/Persistence/IEventRepository.cs
@@ -0,0 +1,8 @@
+﻿using Ticket.TicketManagement.Domain.Entities;
+
+namespace Ticket.TicketManagement.Application.Contracts.Persistence
+{
+    public interface IEventRepository : IAsyncRepository<Event>
+    {
+    }
+}
---
+++ src/Core/Ticket.TicketManagement.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,8 @@
+﻿using Ticket.TicketManagement.Domain.Entities;
+
+namespace Ticket.TicketManagement.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order>
+    {
+    }
+}
