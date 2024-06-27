c31cb33bbaee254835de5382d854502a9c388c46
zangura <zanguraa@gmail.com>
2024-06-27 17:17:11 +0400

    add EmailService, EmailSettings and Email class

---
+++ src/Core/Ticket.TicketManagement.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Ticket.TicketManagement.Application.Models.Mail;
+
+namespace Ticket.TicketManagement.Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmailAsync(Email email);
+    }
+}
--- src/Core/Ticket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ src/Core/Ticket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using MediatR;
+using Ticket.TicketManagement.Application.Contracts.Infrastructure;
 using Ticket.TicketManagement.Application.Contracts.Persistence;
 using Ticket.TicketManagement.Application.Features.Events.Commands.CreateEvent;
+using Ticket.TicketManagement.Application.Models.Mail;
 using Ticket.TicketManagement.Domain.Entities;
 
 namespace Ticket.TicketManagement.Application.Features.Events.Commands.CreateEvent
@@ -10,11 +12,13 @@ namespace Ticket.TicketManagement.Application.Features.Events.Commands.CreateEve
     {
         private readonly IEventRepository _eventRepository;
         private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
 
-        public CreateEventCommandHandler(IEventRepository eventRepository, IMapper mapper)
+        public CreateEventCommandHandler(IEventRepository eventRepository, IMapper mapper, IEmailService emailService)
         {
             _eventRepository = eventRepository;
             _mapper = mapper;
+            _emailService = emailService;
 
         }
 
@@ -31,6 +35,18 @@ namespace Ticket.TicketManagement.Application.Features.Events.Commands.CreateEve
             }
 
             @event = await _eventRepository.AddAsync(@event);
+
+            var email = new Email() { To = "zanguraa@gmail.com", Body = $"A new event was created: {request}", Subject = "A new event was created" };
+
+            try
+            {
+                await _emailService.SendEmailAsync(email);
+            }
+            catch (Exception ex)
+            {
+
+            }
+
             return @event.EventId;
         }
     }
