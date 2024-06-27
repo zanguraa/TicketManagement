edc7ca1e5a5b3e995cd21ec21eb81c3a28c044f5
zangura <zanguraa@gmail.com>
2024-06-27 17:23:32 +0400

    add InfrastructureServiceRegistration class

---
+++ src/Infrastructure/Ticket.TicketManagement.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ticket.TicketManagement.Application.Contracts.Infrastructure;
+using Ticket.TicketManagement.Application.Models.Mail;
+using Ticket.TicketManagement.Infrastructure.Mail;
+
+namespace Ticket.TicketManagement.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<EmailSettings>(configuration.GetSection("EmailSettings"));
+
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
